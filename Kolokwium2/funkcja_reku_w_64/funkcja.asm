6b9a9b12ffc2d836f2bd2cd2a741984c08ce0054
Multicast96 <kamil.dakus@gmail.com>
2018-01-07 17:13:34 +0100

    Funkcja_reku_w_64

---
+++ Kolokwium2/funkcja_reku_w_64/funkcja.asm
@@ -0,0 +1,64 @@
+public funkcja
+
+.data
+jeden dd 1.0
+dwa dd 2.0
+.code
+
+funkcja PROC
+	push rbp
+	mov rbp,rsp
+	push rbx
+
+	finit
+
+	cmp rcx,1
+	jz return_jeden
+	cmp rcx,2
+	jz return_dwa
+
+	;n>2
+	mov rbx,rcx ;n
+	sub rcx,1 ;n-1
+	sub rsp,40 ;shadow space + 8 bajtow wyrownania
+	call funkcja ;w xmmo0 wynik funkcja(n-1)
+	add rsp,40 ;wyrownanie stosu
+
+	mov rax, 3F99999Ah;1.2
+	push rax ;8 bajtow stos
+	fld dword PTR [rsp]
+
+	sub rsp,8 ;16 bajtow stos
+	movups [rsp],xmm0
+	fld dword PTR [rsp] ;funkcja(n-1)
+	fsub ;st(0) = 1.2 - funkcja(n-1)
+
+	mov [rsp], rbx
+	fild qword PTR [rsp] ;n
+	fdiv ;st(0) = (1.2 - funkcja(n-1))/n
+	
+	mov rax,0
+	mov [rsp],rax
+	mov [rsp+8], rax
+	fstp dword PTR [rsp]
+
+	movups xmm0,[rsp]
+	add rsp,16
+	jmp koniec
+
+return_jeden:
+	movss xmm0, jeden
+	jmp koniec
+
+return_dwa:
+	movss xmm0, dwa
+
+koniec:
+
+
+	pop rbx
+	pop rbp
+	ret
+funkcja ENDP
+
+END
---
+++ Kolokwium2/funkcja_reku_w_64/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+float funkcja_64(__int64 n) {
+	if (n == 1)return 1;
+	else if (n == 2) return 2;
+	else return (1.2 - funkcja_64(n - 1)) / n;
+}
+
+extern float funkcja(__int64 n);
+int main() {
+	__int64 n = 10;
+	printf("Dla n=%ld funkcja = %f\n", n, funkcja_64(n));
+	printf("Dla n=%ld funkcja = %f\n", n, funkcja(n));
+}
